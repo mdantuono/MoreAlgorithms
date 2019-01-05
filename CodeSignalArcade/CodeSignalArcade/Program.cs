fbe03f7d1c7b022c14ffb0c83d5e7367be8fa9e2
mdantuono <mikeishere3@gmail.com>
2019-01-05 12:31:07 -0500

    stringsimilarity algorithm and some arcade completed

---
+++ CodeSignalArcade/CodeSignalArcade/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignalArcade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 3, 6, -2, -5, 7, 3 };
+            Console.WriteLine(adjacentElementsProduct(inputArray));
+
+        }
+
+        int add(int param1, int param2)
+        {
+            return (param1 + param2);
+        }
+
+        int centuryFromYear(int year)
+        {
+            if (year <= 100) return 1;
+            if (year % 100 == 0) return year / 100;
+            return year / 100 + 1;
+        }
+
+        bool checkPalindrome(string inputString)
+        {
+            if (string.Join("", inputString.ToCharArray().Reverse()) == inputString) return true;
+            return false;
+        }
+
+        static int adjacentElementsProduct(int[] inputArray)
+        {
+            int highest = -1000;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if(inputArray[i] * inputArray[i + 1] > highest)
+                {
+                    highest = inputArray[i] * inputArray[i + 1];
+                }
+            }
+            return highest;
+        }
+    }
+}
---
+++ StringSimilarities/StringSimilarities/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSimilarities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            int strLength = Convert.ToInt32(Console.ReadLine());
+
+            if (str1.Length != str2.Length)
+            {
+                Console.WriteLine("Make sure string lengths are the same.");
+                return;
+            }
+
+            int[] counts = stringSimilarity(str1, str2, strLength);
+        }
+        static int[] stringSimilarity(string str1, string str2, int strLength)
+        {
+            List<int> counts = new List<int>();
+
+            for (int i = 0; i < strLength; i++)
+            {
+                counts.Add((int)str1[i] - str2[i]);
+            }
+
+            foreach(int num in counts)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine('\n');
+
+            return counts.ToArray();
+        }
+    }
+}
